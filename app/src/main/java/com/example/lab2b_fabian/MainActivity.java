6289a59eff8ad01eff5921c7f87cdbb2262c6c8c
Fabian Harrison <2023622802@student.uitm.edu.my>
2023-11-16 17:55:33 +0800

    - Only Label and Label name remains to be done

--- app/src/main/java/com/example/lab2b_fabian/MainActivity.java
+++ app/src/main/java/com/example/lab2b_fabian/MainActivity.java
@@ -10,8 +10,8 @@ import android.widget.GridView;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
