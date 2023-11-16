1d611567ebd4bf87156b8bb26d819deb07f3aaf6
Fabian Harrison <2023622802@student.uitm.edu.my>
2023-11-16 17:02:17 +0800

    - Added FullImageActivity interactivity (some issues related to imports but it works when settled) Note to self: Pay attention to the names

--- app/src/main/java/com/example/lab2b_fabian/MainActivity.java
+++ app/src/main/java/com/example/lab2b_fabian/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.lab2b_fabian;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.GridView;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,5 +17,14 @@ public class MainActivity extends AppCompatActivity {
 
         GridView gridView = (GridView) findViewById(R.id.gridView);
         gridView.setAdapter(new ImageAdapter(this));
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+        @Override
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id){
+                Intent i = new Intent(getApplicationContext(), FullImageActivity.class);
+                i.putExtra("id", position);
+                startActivity(i);
+            }
+        });
     }
 }
