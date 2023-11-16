09f3750a766bb474daf15738ee33d269641130c5
Fabian Harrison <2023622802@student.uitm.edu.my>
2023-11-16 16:37:40 +0800

    - Apparently the issue was I was using GridList instead of GridView(fixed)

--- app/src/main/java/com/example/lab2b_fabian/ImageAdapter.java
+++ app/src/main/java/com/example/lab2b_fabian/ImageAdapter.java
@@ -1,4 +1,44 @@
 package com.example.lab2b_fabian;
 
-public class ImageAdapter {
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+
+public class ImageAdapter extends BaseAdapter {
+
+    private Context mContext;
+    public Integer[] thumbImages = {
+            R.drawable.pic01,
+            R.drawable.pic02,
+            R.drawable.pic03,
+            R.drawable.pic04,
+            R.drawable.pic05,
+            R.drawable.pic06,
+            R.drawable.pic07,
+            R.drawable.pic08,
+            R.drawable.pic09,
+    };
+
+    public ImageAdapter(Context c){mContext = c;}
+
+    @Override
+    public int getCount(){return thumbImages.length;}
+
+    @Override
+    public Object getItem(int position){return thumbImages[position];}
+
+    @Override
+    public long getItemId(int position){return 0;}
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent){
+        ImageView imageView = new ImageView(mContext);
+        imageView.setLayoutParams(new ViewGroup.LayoutParams(250,250));
+        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        imageView.setPadding(8,8,8,8);
+        imageView.setImageResource(thumbImages[position]);
+        return imageView;
+    }
 }
