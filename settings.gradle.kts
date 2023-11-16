8a1aa135e3a8c2cd9e74fc8b54bdf624dc8d9750
Fabian Harrison <2023622802@student.uitm.edu.my>
2023-11-16 16:12:22 +0800

    - Added override (forgot)

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Lab2b_Fabian"
+include(":app")
+ 
